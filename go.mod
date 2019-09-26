4d77bcd3cabc1a3f6f01f168024f76d909bc0a44
Alex <amoinier@outlook.fr>
2019-09-26 20:23:00 +0200

    discover search in IP

--- go.mod
+++ go.mod
@@ -3,11 +3,17 @@ module github.com/getcasa/plugin-xiaomi
 go 1.12
 
 require (
+	github.com/anvie/port-scanner v0.0.0-20180225151059-8159197d3770
 	github.com/getcasa/sdk v0.0.0-20190923145410-20bbee062dc8
-	github.com/pulento/go-ssdp v0.0.0-20180514024734-4a0ed625a78b // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
+	github.com/labstack/gommon v0.3.0
+	github.com/liamg/furious v0.0.0-20190619180719-b76d3ae59fcc // indirect
+	github.com/pkg/errors v0.8.1
+	github.com/pulento/go-ssdp v0.0.0-20180514024734-4a0ed625a78b
 	github.com/pulento/yeelight v0.0.0-20180827013714-e72aa2e3c4ef
-	github.com/sirupsen/logrus v1.4.2 // indirect
-	golang.org/x/net v0.0.0-20190923162816-aa69164e4478 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/net v0.0.0-20190923162816-aa69164e4478
+	golang.org/x/sys v0.0.0-20190924154521-2837fb4f24fe // indirect
 )
 
 replace github.com/getcasa/sdk v0.0.0-20190923145410-20bbee062dc8 => ../casa-sdk
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"math/rand"
@@ -9,12 +10,18 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
+	portscanner "github.com/anvie/port-scanner"
 	"github.com/getcasa/sdk"
 )
 
-func main() {}
+func main() {
+	OnStart()
+	for true {
+	}
+}
 
 const (
 	discoverMSG = "M-SEARCH * HTTP/1.1\r\n HOST:239.255.255.250:1982\r\n MAN:\"ssdp:discover\"\r\n ST:wifi_bulb\r\n"
@@ -23,7 +30,8 @@ const (
 	timeout = time.Second * 3
 
 	//SSDP discover address
-	ssdpAddr = "239.255.255.250:1982"
+	ssdpAddr     = "239.255.255.250:1982"
+	yeelightPort = 55443
 
 	//CR-LF delimiter
 	crlf = "\r\n"
@@ -52,6 +60,7 @@ var Config = sdk.Configuration{
 var (
 	discover bool
 	lights   []*Yeelight
+	Socket   *net.UDPConn
 )
 
 type (
@@ -95,6 +104,8 @@ type (
 		Name      string
 		Addr      string
 		rnd       *rand.Rand
+		Socket    net.Conn
+		Connected bool
 	}
 )
 
@@ -106,6 +117,7 @@ type Params struct {
 
 // OnStart start UDP server to get Xiaomi data
 func OnStart() {
+	Socket = nil
 	discover = true
 	go Discover()
 
@@ -164,36 +176,176 @@ func OnStop() {
 	discover = false
 }
 
+func containIPAddress(arr []string, search string) bool {
+	for _, addr := range arr {
+		if addr == search {
+			return true
+		}
+	}
+	return false
+}
+
+var wg sync.WaitGroup
+
 //Discover discovers device in local network via ssdp
 func Discover() {
-	var addr string
 
-	for discover {
-		ssdp, _ := net.ResolveUDPAddr("udp4", ssdpAddr)
-		c, _ := net.ListenPacket("udp4", ":0")
-		socket := c.(*net.UDPConn)
-		socket.WriteToUDP([]byte(discoverMSG), ssdp)
-		socket.SetReadDeadline(time.Now().Add(timeout))
-		rsBuf := make([]byte, 1024)
-		size, _, err := socket.ReadFromUDP(rsBuf)
-		if err != nil {
-			// fmt.Println("no devices found")
-		} else {
-			rs := rsBuf[0:size]
-			addr = parseAddr(string(rs))
-			// fmt.Printf("Device with ip %s found\n", addr)
-			if findLightWithAddr(addr) == nil {
-				newyee := New(addr, string(rs))
-				if newyee != nil {
-					lights = append(lights, newyee)
-					newyee.Listen()
-					fmt.Println(newyee)
+	var ipAddresses []string
+	ifaces, err := net.Interfaces()
+	fmt.Println(err)
+
+	for _, iface := range ifaces {
+		addrs, err := iface.Addrs()
+		if err == nil {
+			for _, addr := range addrs {
+				cleanAddr := addr.String()[:strings.Index(addr.String(), "/")]
+				if cleanAddr != "127.0.0.1" && !strings.Contains(cleanAddr, ":") && !net.ParseIP(cleanAddr).IsLoopback() {
+					cleanAddr = addr.String()[:strings.LastIndex(addr.String(), ".")+1]
+					if !containIPAddress(ipAddresses, cleanAddr) {
+						ipAddresses = append(ipAddresses, cleanAddr)
+					}
 				}
 			}
 		}
 	}
 
+	wg.Add(len(ipAddresses) * 255)
+
+	for _, ipAddr := range ipAddresses {
+		for i := 0; i < 255; i++ {
+			go func(i int, ipAddr string) {
+				ip := ipAddr + strconv.Itoa(i)
+				ps := portscanner.NewPortScanner(ip, 500000*time.Microsecond, 4)
+				opened := ps.IsOpen(55443)
+				if opened {
+					fmt.Println(ip)
+					if findLightWithAddr(ip+":"+strconv.Itoa(yeelightPort)) == nil {
+						newyee := New(ip + ":" + strconv.Itoa(yeelightPort))
+						if newyee != nil {
+							newyee.connect()
+							// lights = append(lights, newyee)
+						}
+					}
+				}
+				wg.Done()
+			}(i, ipAddr)
+
+		}
+	}
+	wg.Wait()
 	return
+}
+
+func (y *Yeelight) connect() {
+	var err error
+	notifCh := make(chan *Notification)
+	done := make(chan struct{}, 1)
+	if y.Socket != nil {
+		y.disconnect()
+	}
+	y.Socket, err = net.DialTimeout("tcp", y.Addr, timeout)
+	if err != nil {
+		fmt.Println("Error connection: " + y.Addr)
+		return
+	}
+
+	fmt.Println("Connected: " + y.Addr)
+	y.Connected = true
+
+	on, err := y.GetProp("power", "color_mode", "ct", "rgb", "hue", "sat", "bright")
+	fmt.Println(err)
+	if err == nil {
+		fmt.Printf("Power is %s", on)
+		// y.SetPower(false)
+	}
+
+	go func(c net.Conn) {
+		//make sure connection is closed when method returns
+		defer y.disconnect()
+
+		connReader := bufio.NewReader(c)
+		for {
+			select {
+			case <-done:
+				return
+			default:
+				data, err := connReader.ReadString('\n')
+				if nil == err {
+					var rs Notification
+					fmt.Println(data)
+					json.Unmarshal([]byte(data), &rs)
+					select {
+					case notifCh <- &rs:
+					default:
+						fmt.Println("Channel is full")
+					}
+				}
+			}
+
+		}
+
+	}(y.Socket)
+
+	// fmt.Println(notifCh.Params)
+	// fmt.Println(done)
+}
+
+func (y *Yeelight) disconnect() {
+	y.Socket.Close()
+	y.Socket = nil
+	y.Connected = false
+}
+
+//New creates new device instance for address provided
+func New(addr string) *Yeelight {
+	return &Yeelight{
+		Addr: addr,
+		rnd:  rand.New(rand.NewSource(time.Now().UnixNano())),
+	}
+
+}
+
+// Listen connects to device and listens for NOTIFICATION events
+func (y *Yeelight) Listen() (<-chan *Notification, chan<- struct{}, error) {
+	var err error
+	notifCh := make(chan *Notification)
+	done := make(chan struct{}, 1)
+
+	conn, err := net.Dial("tcp", y.Addr)
+	if err != nil {
+		return nil, nil, fmt.Errorf("cannot connect to %s. %s", y.Addr, err)
+	}
+
+	fmt.Println("Connection established " + y.Addr)
+	go func(c net.Conn) {
+		//make sure connection is closed when method returns
+		defer closeConnection(conn)
+
+		connReader := bufio.NewReader(c)
+		for {
+			select {
+			case <-done:
+
+				return
+			default:
+				data, err := connReader.ReadString('\n')
+				if nil == err {
+					var rs Notification
+					fmt.Println(data)
+					json.Unmarshal([]byte(data), &rs)
+					select {
+					case notifCh <- &rs:
+					default:
+						fmt.Println("Channel is full")
+					}
+				}
+			}
+
+		}
+
+	}(conn)
+
+	return notifCh, done, nil
 }
 
 func findLightWithAddr(addr string) *Yeelight {
@@ -208,61 +360,73 @@ func findLightWithAddr(addr string) *Yeelight {
 	return nil
 }
 
-//New creates new device instance for address provided
-func New(addr string, info string) *Yeelight {
-	if strings.HasSuffix(info, crlf) {
-		info = info + crlf
-	}
-	resp, err := http.ReadResponse(bufio.NewReader(strings.NewReader(info)), nil)
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-	bright, err := strconv.Atoi(resp.Header.Get("BRIGHT"))
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-	ct, err := strconv.Atoi(resp.Header.Get("CT"))
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-	colormode, err := strconv.Atoi(resp.Header.Get("COLOR_MODE"))
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-	rgb, err := strconv.Atoi(resp.Header.Get("RGB"))
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-	hue, err := strconv.Atoi(resp.Header.Get("HUE"))
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-	sat, err := strconv.Atoi(resp.Header.Get("SAT"))
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-	return &Yeelight{
-		ID:        resp.Header.Get("ID"),
-		Model:     resp.Header.Get("MODEL"),
-		Power:     resp.Header.Get("POWER"),
-		Bright:    bright,
-		ColorMode: colormode,
-		CT:        ct,
-		RGB:       rgb,
-		Hue:       hue,
-		Sat:       sat,
-		Name:      resp.Header.Get("NAME"),
-		Addr:      addr,
-		rnd:       rand.New(rand.NewSource(time.Now().UnixNano())),
-	}
+// //New creates new device instance for address provided
+// func New(addr string, info string) *Yeelight {
+// 	if strings.HasSuffix(info, crlf) {
+// 		info = info + crlf
+// 	}
+// 	resp, err := http.ReadResponse(bufio.NewReader(strings.NewReader(info)), nil)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return nil
+// 	}
+// 	bright, err := strconv.Atoi(resp.Header.Get("BRIGHT"))
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return nil
+// 	}
+// 	ct, err := strconv.Atoi(resp.Header.Get("CT"))
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return nil
+// 	}
+// 	colormode, err := strconv.Atoi(resp.Header.Get("COLOR_MODE"))
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return nil
+// 	}
+// 	rgb, err := strconv.Atoi(resp.Header.Get("RGB"))
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return nil
+// 	}
+// 	hue, err := strconv.Atoi(resp.Header.Get("HUE"))
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return nil
+// 	}
+// 	sat, err := strconv.Atoi(resp.Header.Get("SAT"))
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return nil
+// 	}
+// 	return &Yeelight{
+// 		ID:        resp.Header.Get("ID"),
+// 		Model:     resp.Header.Get("MODEL"),
+// 		Power:     resp.Header.Get("POWER"),
+// 		Bright:    bright,
+// 		ColorMode: colormode,
+// 		CT:        ct,
+// 		RGB:       rgb,
+// 		Hue:       hue,
+// 		Sat:       sat,
+// 		Name:      resp.Header.Get("NAME"),
+// 		Addr:      addr,
+// 		rnd:       rand.New(rand.NewSource(time.Now().UnixNano())),
+// 	}
 
+// }
+
+//SetPower is used to switch on or off the smart LED (software managed on/off).
+func (y *Yeelight) SetPower(on bool) error {
+	var status string
+	if on {
+		status = "on"
+	} else {
+		status = "off"
+	}
+	_, err := y.executeCommand("set_power", status)
+	return err
 }
 
 //parseAddr parses address from ssdp response
@@ -288,18 +452,6 @@ func (y *Yeelight) GetProp(values ...interface{}) ([]interface{}, error) {
 	return r.Result, nil
 }
 
-//SetPower is used to switch on or off the smart LED (software managed on/off).
-func (y *Yeelight) SetPower(on bool) error {
-	var status string
-	if on {
-		status = "on"
-	} else {
-		status = "off"
-	}
-	_, err := y.executeCommand("set_power", status)
-	return err
-}
-
 func (y *Yeelight) newCommand(name string, params []interface{}) *Command {
 	return &Command{
 		Method: name,
@@ -316,78 +468,38 @@ func (y *Yeelight) executeCommand(name string, params ...interface{}) (*CommandR
 //executeCommand executes command
 func (y *Yeelight) execute(cmd *Command) (*CommandResult, error) {
 
-	conn, err := net.Dial("tcp", y.Addr)
-	if nil != err {
-		return nil, fmt.Errorf("cannot open connection to %s. %s", y.Addr, err)
+	if !y.Connected || y.Socket == nil {
+		y.connect()
 	}
-	time.Sleep(time.Second)
-	conn.SetReadDeadline(time.Now().Add(timeout))
-
 	//write request/command
 	b, _ := json.Marshal(cmd)
-	fmt.Fprint(conn, string(b)+crlf)
+	// fmt.Fprint(y.Socket, string(b)+crlf)
 
-	//wait and read for response
-	res, err := bufio.NewReader(conn).ReadString('\n')
-	if err != nil {
-		return nil, fmt.Errorf("cannot read command result %s", err)
-	}
-	var rs CommandResult
-	err = json.Unmarshal([]byte(res), &rs)
-	if nil != err {
-		return nil, fmt.Errorf("cannot parse command result %s", err)
-	}
-	if nil != rs.Error {
-		return nil, fmt.Errorf("command execution error. Code: %d, Message: %s", rs.Error.Code, rs.Error.Message)
-	}
-	return &rs, nil
+	b = bytes.Join([][]byte{b, []byte(crlf)}, nil)
+
+	y.Socket.Write(b)
+
+	return nil, nil
+
+	// 	//wait and read for response
+	// 	res, err := bufio.NewReader(y.Socket).ReadString('\n')
+	// 	if err != nil {
+	// 		return nil, fmt.Errorf("cannot read command result %s", err)
+	// 	}
+	// 	var rs CommandResult
+	// 	err = json.Unmarshal([]byte(res), &rs)
+	// 	if nil != err {
+	// 		return nil, fmt.Errorf("cannot parse command result %s", err)
+	// 	}
+	// 	if nil != rs.Error {
+	// 		return nil, fmt.Errorf("command execution error. Code: %d, Message: %s", rs.Error.Code, rs.Error.Message)
+	// 	}
+	// 	return &rs, nil
 }
 
 func (y *Yeelight) randID() int {
 	i := y.rnd.Intn(100)
 	return i
-}
-
-// Listen connects to device and listens for NOTIFICATION events
-func (y *Yeelight) Listen() (<-chan *Notification, chan<- struct{}, error) {
-	var err error
-	notifCh := make(chan *Notification)
-	done := make(chan struct{}, 1)
-
-	conn, err := net.DialTimeout("tcp", y.Addr, time.Second*3)
-	if err != nil {
-		return nil, nil, fmt.Errorf("cannot connect to %s. %s", y.Addr, err)
-	}
-
-	fmt.Println("Connection established")
-	go func(c net.Conn) {
-		//make sure connection is closed when method returns
-		defer closeConnection(conn)
-
-		connReader := bufio.NewReader(c)
-		for {
-			select {
-			case <-done:
-				return
-			default:
-				data, err := connReader.ReadString('\n')
-				if nil == err {
-					var rs Notification
-					fmt.Println(data)
-					json.Unmarshal([]byte(data), &rs)
-					select {
-					case notifCh <- &rs:
-					default:
-						fmt.Println("Channel is full")
-					}
-				}
-			}
-
-		}
-
-	}(conn)
-
-	return notifCh, done, nil
 }
 
 //closeConnection closes network connection
